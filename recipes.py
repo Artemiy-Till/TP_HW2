c5c569147cb9e372c6e4b405c344cd1ed16f8979
Artemiy TIll <aatil@edu.hse.ru>
2026-06-03 22:43:10 +0300

    Добавлен класс ShoppingList

--- recipes.py
+++ recipes.py
@@ -63,3 +63,47 @@ class Recipe:
             result += f"- {ingredient}\n"
 
         return result
+
+class ShoppingList:
+    def __init__(self):
+        self._items = []
+
+    def add_recipe(self, recipe, portions):
+        if portions <= 0:
+            raise ValueError("Количество порций должно быть положительным")
+
+        scaled_recipe = recipe.scale(portions)
+
+        for ingredient in scaled_recipe.ingredients:
+            self._items.append((ingredient, recipe.title))
+
+    def remove_recipe(self, title):
+        self._items = [item for item in self._items if item[1] != title]
+
+    def get_list(self):
+        result = {}
+
+        for ingredient, recipe_title in self._items:
+            key = (ingredient.name, ingredient.unit)
+
+            if key in result:
+                result[key] += ingredient.quantity
+            else:
+                result[key] = ingredient.quantity
+
+        shopping_ingredients = []
+
+        for key, quantity in result.items():
+            name, unit = key
+            shopping_ingredients.append(Ingredient(name, quantity, unit))
+
+        shopping_ingredients.sort(key=lambda ingredient: ingredient.name)
+
+        return shopping_ingredients
+
+    def __add__(self, other):
+        new_list = ShoppingList()
+
+        new_list._items = self._items.copy() + other._items.copy()
+
+        return new_list
